19e2e4d89ca0b4c29010f5f0ed207e61bc40d068
Ameet Kaustav <ameet.kaustav@gmail.com>
2016-05-21 23:22:00 -0400

    Fetching data from YahooAPI and displaying the Stock Details

--- js/app.js
+++ js/app.js
@@ -7,12 +7,13 @@ $(function() {
   function fetchStockDetails(event) {
     event.preventDefault();
     event.stopPropagation ? event.stopPropagation() : (event.cancelBubble=true);
-    var stockSymbol = $('input#stock-symbol-input').val();
+    var symbol = $('input#stock-symbol-input').val();
 
-    var query = 'select * from yahoo.finance.quotes where symbol in ("' + stockSymbol + '")';
     var url = 'http://query.yahooapis.com/v1/public/yql';
+    var query = 'select * from yahoo.finance.quotes where symbol in ("' + symbol + '")';
     var settings = {
       method: 'GET',
+      dataType: 'json',
       data: {
         format: 'json',
         env: 'http://datatables.org/alltables.env',
@@ -21,14 +22,37 @@ $(function() {
     };
 
     $.ajax(url, settings)
-    .done(function() {
-      alert( "success" );
+    .done(function(response) {
+      // Select DOM elements into variables
+      var stockDetailContainer = $('#stock-details');
+      var stockSymbolElement = $('#stock-symbol');
+      var stockValueElement = $('#stock-value');
+      var stockNameElement = $('#stock-name');
+      var stockChangeElement = $('#stock-change');
+      var stockLastTradedElement = $('#stock-last-traded');
+
+      // Set data from response of the API call into variables
+      var stockSymbol = response.query.results.quote.Symbol;
+      var stockValue = response.query.results.quote.LastTradePriceOnly;
+      var stockName = response.query.results.quote.Name;
+      var stockChange = response.query.results.quote.Change;
+      var stockLastTraded = response.query.results.quote.LastTradeDate;
+
+      // Set the text node of the selected DOM elements to the corresponding data from the API call
+      stockSymbolElement.text(stockSymbol);
+      stockValueElement.text(stockValue);
+      stockNameElement.text(stockName);
+      stockChangeElement.text(stockChange);
+      stockLastTradedElement.text(stockLastTraded);
+
+      // Display the Stock Details container
+      stockDetailContainer.removeClass('hidden');
     })
     .fail(function() {
       alert( "error" );
     })
     .always(function() {
-      alert( "complete" );
+      //alert( "complete" );
     });
   }
 
